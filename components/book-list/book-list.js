51d94d84caa53fe4e5864fb2f8e6c9f1523cd955
Your Name <you@example.com>
2021-05-29 23:08:45 +0800

    initial commit

---
+++ components/book-list/book-list.js
@@ -0,0 +1,27 @@
+// components/book-list/book-list.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    bookName: "《人类的故事》",
+    img: "/images/img1.jpg",
+    author: "房龙",
+    publisher: "江苏文艺",
+    date: "2012",
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
